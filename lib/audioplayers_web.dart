3fd3c132e72227efff112146541d50587da195fe
Nick Fisher <nick.fisher@avinium.com>
2020-04-07 22:07:08 +0800

    bump version

--- lib/audioplayers_web.dart
+++ lib/audioplayers_web.dart
@@ -29,7 +29,7 @@ class WrappedPlayer {
     }
   }
 
-  int _int32Divisor = 2147483648;
+  static const int _int32Divisor = 2147483648;
   ByteData _int32ToFloat32(ByteData source, int sampleRate, int numChannels) {
       var newBuf = ByteData(source.lengthInBytes);
       for(int i = 0 ; i < source.lengthInBytes; i+=4) {
@@ -39,12 +39,15 @@ class WrappedPlayer {
       return newBuf;  
   }
 
-  int _int16Divisor = 32768;
+  static const int _int16Divisor = 32768;
   ByteData _int16ToFloat32(ByteData source, int sampleRate, int numChannels) {
     var newBuf = ByteData(source.lengthInBytes * 2);
     for(int i = 0 ; i < source.lengthInBytes;i+=2) {
       int val = source.getInt16(i, Endian.little);
-      newBuf.setFloat32(i,  val / _int16Divisor, Endian.little);
+      double floatVal = val / _int16Divisor;
+
+      newBuf.setFloat32(i*2, floatVal, Endian.little);
+
     }
     return newBuf;  
   }
@@ -52,10 +55,12 @@ class WrappedPlayer {
 
   void playBuffer(ByteBuffer buffer, int sampleRate, int numChannels, int bitDepth) async {
     var view = buffer.asByteData(44);
-    int numFrames = view.lengthInBytes ~/ bitDepth ~/ 8;
+    int numFrames = view.lengthInBytes ~/ (bitDepth ~/ 8) ~/ numChannels; 
+    print("Playing audio buffer of length ${buffer.lengthInBytes}, sample rate $sampleRate, bitDepth $bitDepth and numFrames $numFrames");
     AudioBuffer _audioBuf = _audioCtx.createBuffer(numChannels, numFrames, sampleRate);
     // need to convert from 16/32 bit PCM to 32 bit float
     ByteData converted = bitDepth == 16 ? _int16ToFloat32(view, sampleRate, numChannels) : _int32ToFloat32(view, sampleRate, numChannels);
+
     _audioBuf.copyToChannel(converted.buffer.asFloat32List(), 0);
     setBuffer(_audioBuf);
     start(0);
@@ -71,7 +76,7 @@ class WrappedPlayer {
     currentNode.buffer = currentBuffer;
 
     gainNode = _audioCtx.createGain();
-    gainNode.gain.value = currentVolume;
+
     gainNode.connectNode(_audioCtx.destination);
 
     currentNode.connectNode(gainNode);
@@ -86,6 +91,11 @@ class WrappedPlayer {
       recreateNode();
     }
     startingPoint = _audioCtx.currentTime;
+    gainNode.gain.setValueAtTime(0, startingPoint);
+    gainNode.gain.linearRampToValueAtTime(currentVolume, startingPoint + 0.03);
+    gainNode.gain.setValueAtTime(currentVolume, startingPoint + currentBuffer.duration - 0.03);
+    gainNode.gain.linearRampToValueAtTime(0, startingPoint + currentBuffer.duration);
+
     soughtPosition = position;
     currentNode.start(startingPoint, soughtPosition);
   }
